39f7cc5a3b1c9d734ac691dff1566bb3d2decbdd
Trusov Vladimir <v.trusov@aerodisk.ru>
2024-12-04 14:07:54 +0300

    backups: prepares restic adapter file for adapter class

---
+++ openvair/modules/backup/adapters/restic.py
@@ -0,0 +1,8 @@
+"""Adapter for interacting with restic by executing bash command.
+
+This module provides the `ResticAdapter` class, which is used to manage
+backuping through the restic app.
+
+Classes:
+    ResticAdapter: Adapter class for managing backups.
+"""
