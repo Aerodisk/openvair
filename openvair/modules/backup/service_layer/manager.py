9fb03fe53f385a0105436ddb5c80e2d63e3a8532
Trusov Vladimir <v.trusov@aerodisk.ru>
2024-12-04 16:29:39 +0300

    baclup: creates a service layer files

---
+++ openvair/modules/backup/service_layer/manager.py
@@ -0,0 +1,5 @@
+"""Manager module for backup operations in the service layer.
+
+This module defines the `BackupServiceLayerManager` class, responsible
+for managing backup operations at the service layer.
+"""
