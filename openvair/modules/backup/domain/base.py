9c0ffb43f22fc1de74cd40f41f702203a6491b6b
Trusov Vladimir <v.trusov@aerodisk.ru>
2024-12-13 14:53:20 +0300

    prepares base classes: FSBackuper; DBBackuper

--- openvair/modules/backup/domain/base.py
+++ openvair/modules/backup/domain/base.py
@@ -1,41 +1,42 @@
 """Module for providing abstract base classes for backup domain entities."""
 
 from abc import ABCMeta, abstractmethod
-from typing import Dict, List, Optional
+from typing import Any, Dict, List, Union, Optional
 from pathlib import Path
+
+from pydantic import BaseModel
+
+
+class BackupData(BaseModel):
+    """Base model for data to be backed up."""
+
+    data: Dict[str, Any]  # JSON-compatible structure
+    metadata: Optional[Dict[str, Any]] = None  # Optional metadata
 
 
 class BaseBackuper(metaclass=ABCMeta):
-    """Base class for backup domain entities
-
-    Attributes:
-        repository (Path): Path to the repository with backups
-    """
-
-    def __init__(self, repository: Path) -> None:
-        """Initializes a new backuper entity
-
-        Args:
-            repository (Path): Path to the repository with backups
-        """
-        self.repository = repository
+    """Base class for backup domain entities"""
 
     @abstractmethod
-    def do_backup(
-        self,
-        *,
-        paths: List[Path],
-        tags: Optional[List[str]],
-    ) -> Dict[str, str]:
+    def backup(self) -> Dict[str, Union[str, int]]:
         """For implementing backup logic"""
         ...
 
     @abstractmethod
-    def restore(
-        self,
-        *,
-        paths: List[Path],
-        tags: Optional[List[str]],
-    ) -> Dict[str, str]:
+    def restore(self, data: Dict[str, str]) -> Dict[str, Union[str, int]]:
         """For implementing restore logic"""
         ...
+
+
+class FSBackuper(BaseBackuper):
+    def __init__(self, source_path: str) -> None:
+        self.source_path = Path(source_path)
+
+    @abstractmethod
+    def init_repository(self) -> Dict[str, str]: ...
+
+    @abstractmethod
+    def get_snapshots(self) -> List[Dict]: ...
+
+
+class DBBackuper(BaseBackuper): ...
