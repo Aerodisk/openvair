ce84383f480a93af7328324b8579e8ce4276a2f5
Trusov Vladimir <v.trusov@aerodisk.ru>
2024-12-17 14:46:43 +0300

    changes names for restic params in congif file

--- openvair/modules/backup/config.py
+++ openvair/modules/backup/config.py
@@ -5,5 +5,5 @@ from openvair import config
 API_SERVICE_LAYER_QUEUE_NAME: str = config.RPC_QUEUES.Backup.SERVICE_LAYER
 SERVICE_LAYER_DOMAIN_QUEUE_NAME: str = config.RPC_QUEUES.Backup.DOMAIN_LAYER
 
-RESTIC_DIR: Path = Path(config.data.get('backup', {}).get('restic_dir'))
-RESTIC_PASSWORD: str = config.data.get('backup', {}).get('restic_pass')
+RESTIC_DIR: Path = Path(config.data.get('backup', {}).get('repository'))
+RESTIC_PASSWORD: str = config.data.get('backup', {}).get('password')
