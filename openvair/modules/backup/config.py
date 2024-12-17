dd3c73cb8e714e4f998254631a5e3981a0f27c91
Trusov Vladimir <v.trusov@aerodisk.ru>
2024-12-17 14:47:34 +0300

    adds params into bacup config.py

--- openvair/modules/backup/config.py
+++ openvair/modules/backup/config.py
@@ -7,3 +7,6 @@ SERVICE_LAYER_DOMAIN_QUEUE_NAME: str = config.RPC_QUEUES.Backup.DOMAIN_LAYER
 
 RESTIC_DIR: Path = Path(config.data.get('backup', {}).get('repository'))
 RESTIC_PASSWORD: str = config.data.get('backup', {}).get('password')
+
+DEFAULT_SESSION_FACTORY = config.get_default_session_factory()
+STORAGE_DATA = Path(config.data['storage'].get('data_path', ''))
