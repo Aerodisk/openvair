289c4d3f7bbd88e4797a65b2d7bba5700d2a86ac
TrueVladimir <v.trusov@aerodisk.ru>
2025-04-03 15:50:15 +0300

    moves dto into adapters

--- openvair/modules/template/adapters/serializer.py
+++ openvair/modules/template/adapters/serializer.py
@@ -7,7 +7,7 @@ Classes:
     - TemplateSerializer: Converts Template <-> TemplateDTO
 """
 
-from openvair.modules.template.shared.dto import TemplateDTO
+from openvair.modules.template.adapters.dto import TemplateDTO
 from openvair.modules.template.adapters.orm import Template as TemplateORM
 from openvair.common.serialization.base_serializer import BaseSerializer
 
