c5170e075fc35e7a90c937ba30bca997a1e71e8a
TrueVladimir <v.trusov@aerodisk.ru>
2025-04-10 11:49:11 +0300

    prepares attributes for domain

--- openvair/modules/template/domain/base.py
+++ openvair/modules/template/domain/base.py
@@ -5,11 +5,23 @@ providing a foundation for different template types.
 """
 
 import abc
-from typing import Dict
+from typing import TYPE_CHECKING, Dict
+
+from openvair.libs.qemu_img.adapter import QemuImgAdapter
+
+if TYPE_CHECKING:
+    from pathlib import Path
 
 
 class BaseTemplate(metaclass=abc.ABCMeta):
     """Abstract base class for template models."""
+
+    def __init__(self) -> None:  # noqa: D107
+        self.qemu_img_adapter = QemuImgAdapter()
+        self.format: str = 'qcow2'
+        self.name: str
+        self.path: Path
+        self.is_backing: bool
 
     @abc.abstractmethod
     def create(self) -> Dict:  # noqa: D102
