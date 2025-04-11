524faccfa39ceeccf6c5410bd85e073f998efaae
TrueVladimir <v.trusov@aerodisk.ru>
2025-04-11 09:59:41 +0300

    implements domain logic

--- openvair/modules/template/domain/base.py
+++ openvair/modules/template/domain/base.py
@@ -1,11 +1,12 @@
 """Base classes for template domain models.
 
-This module defines abstract base classes for template-related domain models,
-providing a foundation for different template types.
+This module defines the `BaseTemplate` abstract class, which serves as the
+foundation for implementing specific template types (e.g., QCOW2). It declares
+the required interface and shared fields used for managing templates.
 """
 
 import abc
-from typing import TYPE_CHECKING, Dict
+from typing import TYPE_CHECKING, Dict, List
 
 from openvair.libs.qemu_img.adapter import QemuImgAdapter
 
@@ -14,24 +15,54 @@ if TYPE_CHECKING:
 
 
 class BaseTemplate(metaclass=abc.ABCMeta):
-    """Abstract base class for template models."""
+    """Abstract base class for template domain models.
 
-    def __init__(self) -> None:  # noqa: D107
+    This class defines the interface for all template operations such as
+    creation, editing, and deletion. Concrete implementations must implement
+    all abstract methods.
+
+    Attributes:
+        qemu_img_adapter (QemuImgAdapter): Utility for performing disk-level
+            operations.
+        format (str): Disk format (default: 'qcow2').
+        name (str): Name of the template.
+        path (Path): Filesystem path to the template image.
+        related_volumes (List): List of volumes created from this template.
+        is_backing (bool): Indicates if the template is a backing image.
+    """
+
+    def __init__(self) -> None:
+        """Initialize base attributes for a template.
+
+        Sets up the QEMU image adapter and defines default fields to be used
+        by subclasses (e.g., template format, name, path, related volumes).
+        """
         self.qemu_img_adapter = QemuImgAdapter()
         self.format: str = 'qcow2'
         self.name: str
         self.path: Path
+        self.related_volumes: List
         self.is_backing: bool
 
     @abc.abstractmethod
-    def create(self) -> Dict:  # noqa: D102
+    def create(self, creation_data: Dict) -> None:
+        """Create the template on disk.
+
+        Args:
+            creation_data (Dict): Data required for template creation.
+        """
         ...
 
     @abc.abstractmethod
-    def edit(self) -> Dict:  # noqa: D102
+    def edit(self, editing_data: Dict) -> None:
+        """Edit the template metadata or file.
+
+        Args:
+            editing_data (Dict): Data for modifying the template.
+        """
         ...
 
     @abc.abstractmethod
-    def delete(self) -> Dict:  # noqa: D102
+    def delete(self) -> None:
+        """Delete the template file from disk."""
         ...
-
--- openvair/modules/template/domain/disk_templates/qcow2.py
+++ openvair/modules/template/domain/disk_templates/qcow2.py
@@ -1,17 +1,100 @@
-from typing import Dict  # noqa: D100
+"""QCOW2 template domain logic.
 
+This module provides the concrete implementation of the `BaseTemplate` for
+QCOW2 disk images. It uses `qemu-img` operations to manage template files.
+"""
+
+from typing import Dict
+from pathlib import Path
+
+from openvair.libs.log import get_logger
+from openvair.libs.qemu_img.exceptions import QemuImgError
 from openvair.modules.template.domain.base import BaseTemplate
+from openvair.modules.template.domain.exception import (
+    TemplateFileEditingException,
+    TemplateFileCreatingException,
+    TemplateFileDeletingException,
+)
+
+LOG = get_logger(__name__)
 
 
-class Qcow2Template(BaseTemplate):  # noqa: D101
-    def __init__(self) -> None:  # noqa: D107
+class Qcow2Template(BaseTemplate):
+    """Concrete implementation of `BaseTemplate` for QCOW2 images.
+
+    Provides logic for creating, editing, and deleting template files in
+    `qcow2` format using the `qemu-img` utility.
+    """
+
+    def __init__(self) -> None:
+        """Initialize a QCOW2 template instance.
+
+        Inherits base fields and logic from `BaseTemplate`.
+        """
         super().__init__()
 
-    def create(self) -> Dict:  # noqa: D102
-        raise NotImplementedError
+    def create(self, creation_data: Dict) -> None:
+        """Create a QCOW2 template file from an existing volume.
 
-    def edit(self) -> Dict:  # noqa: D102
-        raise NotImplementedError
+        Args:
+            creation_data (Dict): Must contain 'source_disk_path' (str or Path).
 
-    def delete(self) -> Dict:  # noqa: D102
-        raise NotImplementedError
+        Raises:
+            FileNotFoundError: If the source disk does not exist.
+            FileExistsError: If the target template path already exists.
+            TemplateFileCreatingException: If creation via qemu-img fails.
+        """
+        source_disk_path = creation_data['source_disk_path']
+
+        if not Path(source_disk_path).exists():
+            message = f'Source disk not found: {source_disk_path}'
+            raise FileNotFoundError(message)
+
+        if self.path.exists():
+            message = f'Template already exists at {self.path}'
+            raise FileExistsError(message)
+
+        try:
+            self.qemu_img_adapter.create_copy(source_disk_path, self.path)
+        except QemuImgError as err:
+            LOG.error(
+                f'Error while creating template file: {self.path} '
+                f'from {source_disk_path}'
+            )
+            raise TemplateFileCreatingException(str(self.path)) from err
+
+    def edit(self, editing_data: Dict) -> None:
+        """Rename the template file if not used as a backing image.
+
+        Args:
+            editing_data (Dict): Must contain 'new_name' (str).
+
+        Raises:
+            RuntimeError: If template is in use as a backing image.
+            TemplateFileEditingException: If file renaming fails.
+        """
+        if self.is_backing and self.related_volumes:
+            message = 'Cannot edit template in use'
+            raise TemplateFileEditingException(message)
+
+        new_name = editing_data['new_name']
+        new_path = self.path.parent.absolute() / new_name
+        try:
+            self.path.rename(new_path)
+            self.name = new_name
+            self.path = new_path
+        except OSError as err:
+            LOG.error(f'Error while editing template file: {self.name}')
+            raise TemplateFileEditingException(str(self.name)) from err
+
+    def delete(self) -> None:
+        """Delete the QCOW2 template file.
+
+        Raises:
+            TemplateFileDeletingException: If deletion fails.
+        """
+        try:
+            self.path.unlink()
+        except OSError as err:
+            message = f'Failed to delete template file: {err}'
+            raise TemplateFileDeletingException(message) from err
