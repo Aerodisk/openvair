bf3261f855465bc3e6770010a23a6b1d29ff0f5e
MilenaKhalil <milena.khalil@gmail.com>
2025-08-08 13:29:29 +0300

    Draft

--- openvair/modules/user/tests/api/conftest.py
+++ openvair/modules/user/tests/api/conftest.py
@@ -6,16 +6,22 @@ from fastapi.testclient import TestClient
 from openvair.main import app
 from openvair.libs.auth.jwt_utils import oauth2schema, get_current_user
 
-FIXED_USER_ID = "0b67a738-34ff-4f9e-b0f6-5962065c0207"  # fake user_id
+from uuid import uuid4
 
-class FakeOAuth2Schema:  # noqa: D101
-    async def __call__(self) -> str:  # noqa: D102
+
+@pytest.fixture(scope='session')
+def user_id() -> str:
+    return str(uuid4())
+
+class FakeOAuth2Schema:
+    async def __call__(self):
         return "fake-token"
 
+
 def get_fake_get_current_user(isadmin: bool) -> Any:  # noqa: ANN401, D103, FBT001
-    async def fake_get_current_user() -> Dict[str, Any]:
+    async def fake_get_current_user(user_id: str) -> Dict[str, Any]:
         return {
-            "id": FIXED_USER_ID,
+            "id": user_id,
             "username": "user_for_testing",
             "email": "UUUUser@mail.com",
             "is_superuser": isadmin,
@@ -24,26 +30,15 @@ def get_fake_get_current_user(isadmin: bool) -> Any:  # noqa: ANN401, D103, FBT0
     return fake_get_current_user
 
 @pytest.fixture
-def client_with_auth_admin() -> Any:  # noqa: ANN401, D103
+def client_with_auth_admin(client: TestClient) -> Any:
     app.dependency_overrides[oauth2schema] = FakeOAuth2Schema()
     app.dependency_overrides[get_current_user] = get_fake_get_current_user(True)  # noqa: FBT003
-    client = TestClient(app)
     yield client
     app.dependency_overrides.clear()
 
 @pytest.fixture
-def client_with_auth_not_admin() -> None:  # type: ignore # noqa: D103
+def client_with_auth_not_admin(client: TestClient) -> None:  # type: ignore # noqa: D103
     app.dependency_overrides[oauth2schema] = FakeOAuth2Schema()
     app.dependency_overrides[get_current_user] = get_fake_get_current_user(False)  # noqa: E501, FBT003
-    client = TestClient(app)
     yield client
     app.dependency_overrides.clear()
-
-
-@pytest.fixture
-def authorized_user_admin(client_with_auth_admin: Any) -> Any:  # noqa: ANN401, D103
-    return client_with_auth_admin, FIXED_USER_ID
-
-@pytest.fixture
-def authorized_user_not_admin(client_with_auth_not_admin: Any) -> Any:  # noqa: ANN401, D103
-    return client_with_auth_not_admin, FIXED_USER_ID
--- openvair/modules/user/tests/api/test_create.py
+++ openvair/modules/user/tests/api/test_create.py
@@ -1,55 +1,75 @@
-from typing import Any  # noqa: D100
+from typing import Any, List # noqa: D100
 
+from uuid import uuid4
 from fastapi import status
+import pytest
 from fastapi.testclient import TestClient
 
-from openvair.modules.user.tests.api.conftest import FIXED_USER_ID
 
-
-def test_create_user_authorized_admin(authorized_user_admin: Any) -> None:  # noqa: ANN401, D103
-    client, user_id = authorized_user_admin
-
-    user_data = {
-        "username": "test_user_created",
-        "email": "test_user_created@example.com",
+user_data = {
+        "username": "test_user_created22222",
+        "email": "test_user_created222222@example.com",
         "is_superuser": True,
         "password": "string"
     }
 
-    headers = {"Authorization": "Bearer fake-token"}
+def test_create_user_authorized_admin(client_with_auth_admin: Any, user_id: str) -> None:  # noqa: ANN401, D103
 
-    response = client.post(f"/user/{user_id}/create/", json=user_data, headers=headers)  # noqa: E501
-
+    user_data.pop("email", None)
+    response = client_with_auth_admin.post(f"/user/{user_id}/create/", json=user_data)  # noqa: E501
     assert response.status_code == status.HTTP_201_CREATED
 
-def test_create_user_authorized_not_admin(authorized_user_not_admin: Any) -> None:  # noqa: ANN401, D103, E501
-    client, user_id = authorized_user_not_admin
 
-    user_data = {
-        "username": "test_user_shouldn't_be_created",
-        "email": "test_user_created2@example.com",
-        "is_superuser": True,
-        "password": "string"
-    }
+def test_create_user_authorized_not_admin(client_with_auth_not_admin: TestClient, user_id: str) -> None:  # noqa: ANN401, D103, E501
 
-    headers = {"Authorization": "Bearer fake-token"}
+    user_data.update({"username": "test_user_shouldn't_be_created", "email": "test_user_created2@example.com"})
 
-    response = client.post(f"/user/{user_id}/create/", json=user_data, headers=headers)  # noqa: E501
-
+    response = client_with_auth_not_admin.post(f"/user/{user_id}/create/", json=user_data)  # noqa: E501
     assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
 
 
-def test_create_user_unauthorized(unauthorized_client: TestClient) -> None:  # noqa: D103
+def test_create_user_unauthorized(unauthorized_client: TestClient, user_id: str) -> None:  # noqa: D103
 
-    user_data = {
-        "username": "un_autorised_attampt_user",
-        "email": "test_user_created3@example.com",
-        "is_superuser": True,
-        "password": "string"
-    }
+    user_data.update({"username": "un_autorised_attampt_user", "email": "test_user_created3@example.com"})
 
-    headers = {"Authorization": "Bearer fake-token"}
-
-    response = unauthorized_client.post(f"/user/{FIXED_USER_ID}/create/", json=user_data, headers=headers)  # noqa: E501
-
+    response = unauthorized_client.post(f"/user/{user_id}/create/", json=user_data)  # noqa: E501
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_create_user_random_uuid(client_with_auth_not_admin: TestClient) -> None:  # noqa: D103
+
+    user_data.update({"username": "un_autorised_attampt_user2", "email": "test_user_created4@example.com"})
+
+    response = client_with_auth_not_admin.post(f"/user/{str(uuid4())}/create/", json=user_data)  # noqa: E501
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.parametrize(
+    'missing_field', ['username', 'email', 'is_superuser', 'password']
+)
+def test_missing_fields(client_with_auth_admin: TestClient, user_id: str, missing_field: str) -> None:  # noqa: ANN401, D103
+
+    user_data.update({"username": "un_autorised_attampt_user4", "email": "test_user_created6@example.com"})
+    incorrect_data = user_data.copy()
+    incorrect_data.pop(missing_field)
+
+    response = client_with_auth_admin.post(f"/user/{user_id}/create/", json=incorrect_data)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+# @pytest.mark.parametrize(
+#     'wrong_params', [
+#         ["email", "qwerty"],
+#         ["username", 22],
+#         ["isadmin", "admin"],
+#         ["password", True]
+#     ]
+# )
+# def test_incorrect_fields(client_with_auth_admin: TestClient, user_id: str, wrong_params: Any) -> None:  # noqa: ANN401, D103
+
+#     user_data.update({"username": "un_autorised_attampt_user44", "email": "test_user_creat4ed6@example.com"})
+#     incorrect_data = user_data.copy()
+#     incorrect_data.update({wrong_params[0], wrong_params[1]})
+
+#     response = client_with_auth_admin.post(f"/user/{user_id}/create/", json=incorrect_data)
+#     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
