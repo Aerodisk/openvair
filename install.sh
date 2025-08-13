48d2c3c64a1f8e4505dfb8c3744188ecdee50d56
tihon49 <tihon49@gmail.com>
2025-08-13 17:22:43 +0300

    refactor: update uv installation method to use official installer

--- install.sh
+++ install.sh
@@ -694,8 +694,8 @@ install_restic(){
 }
 
 install_uv(){
-  local install_uv_command="sudo python3 -m pip install -U uv"
-  local install_message="Installing uv"
+  local install_uv_command="curl -LsSf https://astral.sh/uv/install.sh | sh"
+  local install_message="Installing uv via official installer"
   execute "$install_uv_command" "$install_message"
 }
 
