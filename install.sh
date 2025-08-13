5c4f6984e5c61bdf2380165f1751c8b13d137809
Vladimir <88665954+VladimirTrue@users.noreply.github.com>
2025-08-13 16:40:27 +0300

    fixes type in install_uv func

--- install.sh
+++ install.sh
@@ -696,7 +696,7 @@ install_restic(){
 install_uv(){
   local install_uv_command="sudo python3 -m pip install -U uv"
   local install_message="Installing uv"
-  execute "$install_uv_command" "$insatll_message"
+  execute "$install_uv_command" "$install_message"
 }
 
 install_documentation(){
