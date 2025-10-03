12697c7ad0c403c7c94e68d8654b380f6c3afb24
MilenaKhalil <milena.khalil@gmail.com>
2025-10-03 17:27:55 +0300

    added bridge

--- openvair/modules/virtual_network/tests/api/test_create.py
+++ openvair/modules/virtual_network/tests/api/test_create.py
@@ -7,6 +7,7 @@ Covers:
 - Unauthorized access (if middleware applies globally)
 """
 
+from typing import Dict
 from fastapi import status
 from fastapi.testclient import TestClient
 
@@ -17,66 +18,66 @@ from openvair.modules.virtual_network.entrypoints.schemas import (
 )
 
 
-def test_create_virtual_network_success(client: TestClient) -> None:
+# def test_create_virtual_network_success(client: TestClient, bridge: Dict) -> None:
+#     """Test successful virtual network creation."""
+#     payload = VirtualNetwork(
+#         network_name=generate_test_entity_name('vnet'),
+#         forward_mode='bridge',
+#         bridge=bridge['name'],
+#         virtual_port_type='openvswitch',
+#         port_groups=[
+#             PortGroup(port_group_name='trunk_pg', is_trunk='yes', tags=['100'])
+#         ],
+#     ).model_dump(mode='json')
+
+#     response = client.post('/virtual_networks/create/', json=payload)
+#     assert response.status_code == status.HTTP_200_OK, response.text
+#     data = response.json()
+#     assert data['network_name'] == payload['network_name']
+#     assert data['forward_mode'] == payload['forward_mode']
+#     assert data['bridge'] == payload['bridge']
+#     assert data['virtual_port_type'] == payload['virtual_port_type']
+#     assert data['port_groups'][0]['port_group_name'] == 'trunk_pg'
+
+
+# def test_create_virtual_network_empty_port_groups(client: TestClient, bridge: Dict) -> None:
+#     """port_groups must not be empty."""
+#     payload = VirtualNetwork(
+#         network_name=generate_test_entity_name('vnet'),
+#         forward_mode='bridge',
+#         bridge=bridge['name'],
+#         virtual_port_type='openvswitch',
+#         port_groups=[],
+#     ).model_dump(mode='json')
+
+#     response = client.post('/virtual_networks/create/', json=payload)
+#     assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+# def test_create_virtual_network_missing_fields(client: TestClient, bridge: Dict) -> None:
+#     """port_groups must not be empty."""
+#     payload = VirtualNetwork(
+#         network_name=generate_test_entity_name('vnet'),
+#         forward_mode='bridge',
+#         bridge=bridge['name'],
+#         virtual_port_type='openvswitch',
+#         port_groups=[
+#             PortGroup(port_group_name='trunk_pg', is_trunk='yes', tags=['100'])
+#         ],
+#     ).model_dump(mode='json')
+#     payload.pop('port_groups')
+
+#     response = client.post('/virtual_networks/create/', json=payload)
+#     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_create_virtual_network_unauthorized(bridge: Dict, unauthorized_client: TestClient) -> None:
     """Test successful virtual network creation."""
     payload = VirtualNetwork(
         network_name=generate_test_entity_name('vnet'),
         forward_mode='bridge',
-        bridge='br-int',
-        virtual_port_type='openvswitch',
-        port_groups=[
-            PortGroup(port_group_name='trunk_pg', is_trunk='yes', tags=['100'])
-        ],
-    ).model_dump(mode='json')
-
-    response = client.post('/virtual_networks/create/', json=payload)
-    assert response.status_code == status.HTTP_200_OK, response.text
-
-    data = response.json()
-    assert data['network_name'] == payload['network_name']
-    assert data['forward_mode'] == payload['forward_mode']
-    assert data['bridge'] == payload['bridge']
-    assert data['virtual_port_type'] == payload['virtual_port_type']
-    assert data['port_groups'][0]['port_group_name'] == 'trunk_pg'
-
-
-def test_create_virtual_network_empty_port_groups(client: TestClient) -> None:
-    """port_groups must not be empty."""
-    payload = VirtualNetwork(
-        network_name=generate_test_entity_name('vnet'),
-        forward_mode='bridge',
-        bridge='br-int',
-        virtual_port_type='openvswitch',
-        port_groups=[],
-    ).model_dump(mode='json')
-
-    response = client.post('/virtual_networks/create/', json=payload)
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-
-
-def test_create_virtual_network_missing_fields(client: TestClient) -> None:
-    """port_groups must not be empty."""
-    payload = VirtualNetwork(
-        network_name=generate_test_entity_name('vnet'),
-        forward_mode='bridge',
-        bridge='br-int',
-        virtual_port_type='openvswitch',
-        port_groups=[
-            PortGroup(port_group_name='trunk_pg', is_trunk='yes', tags=['100'])
-        ],
-    ).model_dump(mode='json')
-    payload.pop('port_groups')
-
-    response = client.post('/virtual_networks/create/', json=payload)
-    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-
-
-def test_create_virtual_network_unauthorized(unauthorized_client: TestClient) -> None:
-    """Test successful virtual network creation."""
-    payload = VirtualNetwork(
-        network_name=generate_test_entity_name('vnet'),
-        forward_mode='bridge',
-        bridge='br-int',
+        # bridge="name",
+        bridge=bridge.get('name'),
         virtual_port_type='openvswitch',
         port_groups=[
             PortGroup(port_group_name='trunk_pg', is_trunk='yes', tags=['100'])
@@ -85,20 +86,3 @@ def test_create_virtual_network_unauthorized(unauthorized_client: TestClient) ->
 
     response = unauthorized_client.post('/virtual_networks/create/', json=payload)
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-
-# def test_create_virtual_network_forbidden_bridge(client: TestClient) -> None:
-#     """Bridge 'virbr0' is explicitly forbidden by validator."""
-#     payload = VirtualNetwork(
-#         network_name=generate_test_entity_name('vnet'),
-#         forward_mode='bridge',
-#         bridge='br-test',
-#         virtual_port_type='openvswitch',
-#         port_groups=[PortGroup(is_trunk='no', tags=['100'])],
-#     ).model_dump(mode='json')
-
-#     # Force invalid bridge value
-#     payload['bridge'] = 'virbr0'
-
-#     response = client.post('/virtual_networks/create/', json=payload)
-#     assert response.status_code == status.HTTP_400_BAD_REQUEST
--- openvair/modules/virtual_network/tests/conftest.py
+++ openvair/modules/virtual_network/tests/conftest.py
@@ -12,37 +12,15 @@ from openvair.modules.virtual_network.entrypoints.schemas import (
     PortGroup,
     VirtualNetwork,
 )
-from openvair.modules.network.entrypoints.schemas import BridgeCreate
 
-# @pytest.fixture(scope='function')
-# def bridge(client: TestClient) -> Generator[Dict, None, None]:
-#     """Create a test virtual network and delete it after each test."""
-#     payload = BridgeCreate(
-#         name = generate_test_entity_name('bridge')
-#         type = 'bridge'
-#         interfaces = 
-#     ).model_dump(mode='json')
-
-#     created = create_resource(
-#         client, '/virtual_networks/create/', payload, 'virtual_network'
-#     )
-
-#     yield created
-
-#     delete_resource(
-#         client,
-#         '/virtual_networks',
-#         created['id'],
-#         'virtual_network'
-#     )
 
 @pytest.fixture(scope='function')
-def virtual_network(client: TestClient) -> Generator[Dict, None, None]:
+def virtual_network(client: TestClient, bridge: Dict) -> Generator[Dict, None, None]:
     """Create a test virtual network and delete it after each test."""
     payload = VirtualNetwork(
         network_name=generate_test_entity_name('vnet'),
         forward_mode='bridge',
-        bridge='br-int',  # test bridge name
+        bridge=bridge['name'],
         virtual_port_type='openvswitch',
         port_groups=[
             PortGroup(port_group_name='pg', is_trunk='yes', tags=['100'])
